2f89fe6a054e761aca3cd5929c2c69aff1b9594d
mdlawrenceusa <mdlawrence@me.com>
2014-10-07 16:07:16 -0400

    Initial Commit
    Initial Commit of Visual Basic Lesson 2 Project

---
+++ et502/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub MonthCalendar1_DateChanged(sender As Object, e As DateRangeEventArgs)
+
+    End Sub
+End Class
